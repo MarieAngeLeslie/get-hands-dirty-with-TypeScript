c0354fcbbd08178524979e3fb519e0e4092d2222
MarieAngeLeslie <marieangeleslie0305@gmail.com>
2024-01-07 14:20:22 +0000

    feat:notions on Narrowing - alias&generics

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,23 @@
+"use strict";
+const user = { firstname: "ashraf", lastname: "akimi" };
+function identity(arg) {
+    return arg;
+}
+//Type extends { length: number } : doit étendre d'un objet qui contiendrais une clé length qui serait de type number
+function consoleSize(arg) {
+    console.log(arg.length);
+    return arg;
+}
+const logSize = consoleSize("t");
+const identityVariable = identity(3);
 const compteur = document.querySelector('#compteur');
 let i = 0;
 const increment = (e) => {
+    e.preventDefault();
     i++;
-    compteur.querySelector("span").innerText = i.toString();
+    const span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector("span");
+    if (span) {
+        span.innerText = i.toString();
+    }
 };
-compteur.addEventListener('click', increment);
+compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,36 @@
+type User = { firstname: string, lastname: string }
+type IdentityType<ArgType> = (arg: ArgType) => ArgType
+
+const user: User = { firstname: "ashraf", lastname: "akimi" }
+
+function identity<ArgType>(arg: ArgType): ArgType {
+    return arg;
+}
+
+
+
+//Type extends { length: number } : doit étendre d'un objet qui contiendrais une clé length qui serait de type number
+function consoleSize<Type extends { length: number }>(arg: Type): Type {
+    console.log(arg.length);
+    return arg;
+}
+
+const logSize = consoleSize("t");
+
+const identityVariable = identity(3);
+
 const compteur = document.querySelector('#compteur');
 let i = 0;
 
-const increment = (e) => {
+const increment = (e: Event) => {
+    e.preventDefault();
     i++;
-    compteur.querySelector("span").innerText = i.toString();
+    const span = compteur?.querySelector("span");
+    if (span) {
+        span.innerText = i.toString();
+    }
 }
 
-compteur.addEventListener('click', increment)
+
+
+compteur?.addEventListener('click', increment)
